eaf5c4b263d007feb56b525bbf9c760a09bf8fb7
PedroxVA <PedroxVA@gmail.com>
2023-08-18 16:35:04 -0300

    Create E1.java

---
+++ src/E1_ListaDeTareasPendientes/E1.java
@@ -0,0 +1,19 @@
+
+package E1_ListaDeTareasPendientes;
+
+/* @author PedroxVA */
+public class E1 {
+/*
+    Ejercicios Extras:
+Ejercicio 1:
+Desarrolla una aplicación de lista de tareas pendientes utilizando interfaces gráficas y colecciones
+en Java. La interfaz gráfica debe permitir al usuario agregar nuevas tareas, marcar tareas como
+completadas y eliminar tareas de la lista. Además, la aplicación debe mostrar la lista de tareas
+pendientes y la lista de tareas completadas. Utiliza una colección adecuada para almacenar las
+tareas y asegúrate de que la interfaz gráfica se actualice correctamente cuando se realicen cambios
+en la lista.
+Es importante que repartan las tareas entre los miembros del equipo, de tal forma que: uno crea
+repositorio en GitHub; los demás clonan, luego cada uno hace una de las clases que forman parte
+del proyecto y suben al repositorio los cambios.
+    */
+}
